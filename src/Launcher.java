8bd38858bbaa1ff6d251edc31a36cbe1cb1fb3b2
intplus <intplus2@gmail.com>
2016-06-16 22:51:12 +0300

    Initial commit

---
+++ src/Launcher.java
@@ -0,0 +1,9 @@
+/**
+ * Created by alpo123 on 16.06.16.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        String str = "Hello world";
+        System.out.println(str);
+    }
+}
