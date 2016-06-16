c67a4a2586c0a2732dcc88631461860637d20274
intplus <intplus2@gmail.com>
2016-06-16 22:57:17 +0300

    next commit

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,9 +1,10 @@
-/**
- * Created by alpo123 on 16.06.16.
- */
+
 public class Launcher {
     public static void main(String[] args) {
-        String str = "Hello world";
+        print("Hello world");
+    }
+
+    static void print(String str) {
         System.out.println(str);
     }
 }
